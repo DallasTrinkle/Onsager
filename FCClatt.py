15f16eaff6a78f974fdb80db8824ce83ff9e6494
DallasTrinkle <dtrinkle@illinois.edu>
2014-05-28 11:16:55 -0500

    Added inversion, and tests.

--- FCClatt.py
+++ FCClatt.py
@@ -4,25 +4,7 @@
 
 import numpy as np
 
-def hashindex(n, N):
-    """
-    Hashing function, designed to make generation of 3D lattice map easily to vector.
-    On a face-centerd cubic lattice: atom position (n0/N,n1/N,n2/N) where n0+n1+n2 = even
-    This hash converts lattice position into unique integer from 0 .. N^3/2-1
-
-    Parameters
-    ----------
-    n: three vector of coordinates, in units of 2*a0
-    N: size of box on a side
-
-    Notes
-    -----
-    Assumes that N is even, and that sum(n) is also even, and that all are integers;
-    doesn't check.
-    """
-    return N*N*(n[0]/2) + N*n[1] + n[2]
-
-def makeNNvect():
+def NNvect():
     """
     Constructs the FCC <110> transition vectors.
     """
@@ -38,7 +20,24 @@ def makeNNvect():
                     z+=1
     return NNvect
 
-def makeneighlist (N, NNvect):
+def invlist(NNvect):
+    """
+    Constructs the list of inverse vectors for each vector.
+    Returns invlist[] where, for each k1, nnlist[k1] = -nnlist[invlist[k1]]
+
+    Parameters
+    ----------
+    nnlist: list of nearest neighbor vectors
+    """    
+    z = np.shape(NNvect)[0]
+    invlist = np.empty((z), dtype=int)
+    for k1 in xrange(z):
+        for k2 in xrange(z):
+            if all((NNvect[k1] + NNvect[k2]) == (0,0,0)) :
+                invlist[k1] = k2
+    return invlist
+
+def neighlist (N, NNvect):
     """
     Constructs the full neighbor list for an NxNxN cell, using NNvect as the
     transition vectors; with full periodic boundary conditions.
@@ -64,4 +63,22 @@ def makeneighlist (N, NNvect):
                     for nn in xrange(z):
                         neighlist[nind,nn] = hashindex((n+NNvect[nn])%N, N)
     return neighlist
+
+def hashindex(n, N):
+    """
+    Hashing function, designed to make generation of 3D lattice map easily to vector.
+    On a face-centerd cubic lattice: atom position (n0/N,n1/N,n2/N) where n0+n1+n2 = even
+    This hash converts lattice position into unique integer from 0 .. N^3/2-1
+
+    Parameters
+    ----------
+    n: three vector of coordinates, in units of 2*a0
+    N: size of box on a side
+
+    Notes
+    -----
+    Assumes that N is even, and that sum(n) is also even, and that all are integers;
+    doesn't check.
+    """
+    return N*N*(n[0]/2) + N*n[1] + n[2]
 
--- tests.py
+++ tests.py
@@ -9,17 +9,20 @@ import FCClatt
 
 class LatticeTests(unittest.TestCase):
     def setUp(self):
-        self.nnvect = FCClatt.makeNNvect()
-        self.invlist = FCClatt.invlist(self.nnvect)
+        self.NNvect = FCClatt.NNvect()
+        self.invlist = FCClatt.invlist(self.NNvect)
 
     def testFCCcount(self):
         # check that we have z=12 neighbors, and we're in 3D        
-        self.assertEqual(np.shape(self.nnvect), (12,3))
+        self.assertEqual(np.shape(self.NNvect), (12,3))
 
+    def testFCCinversioncount(self):
+        self.assertEqual(np.shape(self.NNvect)[0], np.shape(self.invlist)[0])
+        
     def testFCCinversion(self):
         # check that for each NN vector, we have its inverse too
         for k1, k2 in enumerate(self.invlist):
-            self.assertEqual(self.nnvect[k1], -self.nnvect[k2])
+            self.assertTrue(all(self.NNvect[k1] == -self.NNvect[k2]))
 
 def main():
     unittest.main()
