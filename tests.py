26a0a0378bdaac53d23d30e2fa670e15322c3d4a
DallasTrinkle <dtrinkle@illinois.edu>
2014-05-28 11:34:08 -0500

    Expanded FCC lattice tests.

--- tests.py
+++ tests.py
@@ -17,13 +17,36 @@ class LatticeTests(unittest.TestCase):
         self.assertEqual(np.shape(self.NNvect), (12,3))
 
     def testFCCinversioncount(self):
+        # right dimensions?
         self.assertEqual(np.shape(self.NNvect)[0], np.shape(self.invlist)[0])
+
+    def testFCCbasic(self):
+        # do we have the right <110> nearest neighbor vectors?
+        self.assertTrue(any( all((1,1,0)==x) for x in self.NNvect ))
+        self.assertTrue(any( all((1,-1,0)==x) for x in self.NNvect ))
+        self.assertTrue(any( all((0,1,1)==x) for x in self.NNvect ))
+        self.assertTrue(any( all((0,1,-1)==x) for x in self.NNvect ))
+        self.assertTrue(any( all((1,0,1)==x) for x in self.NNvect ))
+        self.assertTrue(any( all((-1,0,1)==x) for x in self.NNvect ))
+        self.assertFalse(any( all((0,0,0)==x) for x in self.NNvect ))
+        self.assertFalse(any( all((1,1,1)==x) for x in self.NNvect ))
         
     def testFCCinversion(self):
         # check that for each NN vector, we have its inverse too
         for k1, k2 in enumerate(self.invlist):
             self.assertTrue(all(self.NNvect[k1] == -self.NNvect[k2]))
 
+# At some point, we'll probably want to included point group operations, and appropriate tests...
+
+# import GFcalc
+
+class GreenFuncFourierTransformTests(unittest.TestCase):
+    def setUp(self):
+        self.NNvect = FCClatt.NNvect()
+
+    def testFTdim(self):
+        pass
+            
 def main():
     unittest.main()
 
