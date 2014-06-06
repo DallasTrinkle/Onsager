03bc8613449878f60ba7d0598e17e6b2d6527509
DallasTrinkle <dtrinkle@illinois.edu>
2014-06-05 23:49:54 -0500

    Cleaning up code.

--- KPTmesh.py
+++ KPTmesh.py
@@ -47,20 +47,19 @@ class KPTmesh:
         Nmesh : list
             should have length 3; specifies number of divisions in 1, 2, 3 directions.
         """
-        if Nmesh[0] == self.Nmesh[0] and Nmesh[1] == self.Nmesh[1] and Nmesh[2] == self.Nmesh[2] : return
+        if Nmesh == self.Nmesh : return
         self.Nmesh = Nmesh
         self.Nkpt = np.product(Nmesh)
         if self.Nkpt == 0: return
         dN = np.array([1./x for x in Nmesh])
-        meshrange = [xrange(-Nmesh[0]/2+1, Nmesh[0]/2+1),
-                     xrange(-Nmesh[1]/2+1, Nmesh[1]/2+1),
-                     xrange(-Nmesh[2]/2+1, Nmesh[2]/2+1)]
         # use a list comprehension to iterate and build:
-        self.kptfull = np.array([ np.dot(self.rlattice, (n0*dN[0], n1*dN[1], n2*dN[2]))
-                                  for n0 in meshrange[0] for n1 in meshrange[1] for n2 in meshrange[2] ])
+        self.kptfull = np.array([np.dot(self.rlattice, (n0*dN[0], n1*dN[1], n2*dN[2]))
+                                 for n0 in xrange(-Nmesh[0]/2+1, Nmesh[0]/2+1)
+                                 for n1 in xrange(-Nmesh[1]/2+1, Nmesh[1]/2+1)
+                                 for n2 in xrange(-Nmesh[2]/2+1, Nmesh[2]/2+1)])
         # run through list to ensure that all k-points are inside the BZ
         Gmin = min([ np.dot(G, G) for G in self.BZG])
-        for i,k in enumerate(self.kptfull):
+        for i, k in enumerate(self.kptfull):
             if np.dot(k, k)>=Gmin:
                 for G in self.BZG:
                     if np.dot(k, G)>np.dot(G, G):
@@ -106,13 +105,13 @@ class KPTmesh:
         """
         # Start with a list of possible vectors; add those that define the BZ...
         BZG = []
-        nv = [0, 0, 0]
-        for nv[0] in xrange(-3, 4):
-            for nv[1] in xrange(-3, 4):
-                for nv[2] in xrange(-3, 4):
-                    if nv==[0, 0, 0]: continue
-                    vec = np.dot(self.lattice, nv)
-                    if self.incell(vec, BZG, threshold=0): BZG.append(np.dot(self.rlattice, nv))
+        for nv in [[n0, n1, n2]
+                   for n0 in xrange(-3, 4)
+                   for n1 in xrange(-3, 4)
+                   for n2 in xrange(-3, 4)
+                   if (n0, n1, n2) != (0, 0, 0)]:
+            vec = np.dot(self.lattice, nv)
+            if self.incell(vec, BZG, threshold=0): BZG.append(np.dot(self.rlattice, nv))
         # ... and use a list comprehension to only keep those that still remain
         self.BZG = np.array([0.5*vec for vec in BZG if self.incell(vec, BZG, threshold=0)])
         
