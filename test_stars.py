bc85861bb1cbcafd84ed70ab2bfddbf482156db3
DallasTrinkle <dtrinkle@illinois.edu>
2014-06-15 13:19:54 -0500

    Added testing of star generation against FCC, up to 3rd nn with count.

--- test_stars.py
+++ test_stars.py
@@ -27,10 +27,35 @@ class StarTests(unittest.TestCase):
     def testStarcount(self):
         """Check that the counts (Npts, Nstars) make sense for FCC, with Nshells = 1, 2"""
         self.star.generate(1)
+        # 110
         self.assertEqual(self.star.Nstars, 1)
         self.assertEqual(self.star.Npts, np.shape(self.NNvect)[0])
+        self.assertEqual(self.star.Npts, sum([len(s) for s in self.star.stars]))
         self.star.generate(2)
+        # 110, 200, 211, 220
         self.assertEqual(self.star.Nstars, 4)
+        self.assertEqual(self.star.Npts, sum([len(s) for s in self.star.stars]))
+        for s in self.star.stars:
+            x = s[0]
+            num = (2 ** (3 - list(x).count(0)))
+            if x[0] != x[1] and x[1] != x[2]:
+                num *= 6
+            elif x[0] != x[1] or x[1] != x[2]:
+                num *= 3
+            self.assertEqual(num, len(s))
+        self.star.generate(3)
+        # 110, 200, 211, 220, 310, 321, 330, 222
+        self.assertEqual(self.star.Nstars, 8)
+        self.assertEqual(self.star.Npts, sum([len(s) for s in self.star.stars]))
+        for s in self.star.stars:
+            x = s[0]
+            num = (2 ** (3 - list(x).count(0)))
+            if x[0] != x[1] and x[1] != x[2]:
+                num *= 6
+            elif x[0] != x[1] or x[1] != x[2]:
+                num *= 3
+            self.assertEqual(num, len(s))
+
 
     def testStarmembers(self):
         """Are the members correct?"""
