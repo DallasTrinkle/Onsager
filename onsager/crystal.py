57689414b5a419e4cf630b82cb7e13c8a0e0dbcc
DallasTrinkle <dtrinkle@illinois.edu>
2015-01-18 17:13:01 -0600

    Testing for incell and inhalf. Also started to test "reduce".

--- onsager/crystal.py
+++ onsager/crystal.py
@@ -12,6 +12,18 @@ __author__ = 'Dallas R. Trinkle'
 
 import numpy as np
 
+def incell(vec):
+    """
+    Returns the vector inside the unit cell (in [0,1)**3)
+    """
+    return vec - np.floor(vec)
+
+def inhalf(vec):
+    """
+    Returns the vector inside the centered cell (in [-0.5,0.5)**3)
+    """
+    return vec - np.rint(vec)
+
 class Crystal(object):
     """
     A class that defines a crystal, as well as the symmetry analysis that goes along with it.
@@ -45,23 +57,66 @@ class Crystal(object):
         assert self.lattice.shape == (3,3), "lattice contains vectors that are not 3 dimensional"
         assert type(basis) is list, "basis needs to be a list or list of lists"
         if type(basis[0]) == np.ndarray:
-            for v in basis:
-                assert type(v) is np.ndarray, "{} in {} is not an array".format(v, basis)
-            self.basis = [basis]
+            for u in basis:
+                assert type(u) is np.ndarray, "{} in {} is not an array".format(u, basis)
+            self.basis = [ [incell(u) for u in basis] ]
         else:
             for elem in basis:
                 assert type(elem) is list, "{} in basis is not a list".format(elem)
-                for v in elem:
-                    assert type(v) is np.ndarray, "{} in {} is not an array".format(v, elem)
-            self.basis = basis
+                for u in elem:
+                    assert type(u) is np.ndarray, "{} in {} is not an array".format(u, elem)
+            self.basis = [ [ incell(u) for u in atombasis] for atombasis in basis]
         self.reduce()
         self.calcmetric()
 
     def reduce(self):
         """
-        Reduces the lattice and basis, if needed.
+        Reduces the lattice and basis, if needed. Works recursively.
         """
-        return
+        # Work with the shortest possible list first
+        maxlen = 0
+        atomindex = 0
+        for i, ulist in enumerate(self.basis):
+            if len(ulist) > maxlen:
+                maxlen = len(ulist)
+                atomindex = i
+        if maxlen == 1:
+            return
+        # We need to first check against reducibility of atomic positions: try out non-trivial displacements
+        initpos = self.basis[atomindex][0]
+        for newpos in self.basis[atomindex]:
+            t = newpos - initpos
+            if np.all(t == 0): continue
+            trans = True
+            for atomlist in self.basis:
+                for u in atomlist:
+                    if np.all([ not np.all(np.isclose(inhalf(u+t-v), 0)) for v in atomlist]):
+                        trans = False
+                        break
+            if trans:
+                break
+        if not trans:
+            return
+        # reduce that lattice and basis
+        # 1. determine what the new lattice needs to look like.
+        for d in xrange(3):
+            super = np.eye(3)
+            super[:,d] = t[:]
+            if np.linalg.det(super) != 0:
+                break
+        invsuper = np.linalg.inv(super)
+        self.lattice = np.dot(self.lattice, super)
+        # 2. update the basis
+        newbasis = []
+        for atomlist in self.basis:
+            newatomlist = []
+            for u in atomlist:
+                v = incell(np.dot(invsuper, u))
+                if np.all([ not np.all(np.isclose(v, v1)) for v1 in newatomlist]):
+                    newatomlist.append(v)
+            newbasis.append(newatomlist)
+        self.basis = newbasis
+        self.reduce()
 
     def calcmetric(self):
         """
--- test/test_crystal.py
+++ test/test_crystal.py
@@ -8,6 +8,21 @@ import unittest
 import numpy as np
 import onsager.crystal as crystal
 
+class UnitCellTests(unittest.TestCase):
+    """Tests to make sure incell and halfcell work as expected."""
+    def testincell(self):
+        """In cell testing"""
+        a = np.array([4./3., -2./3.,19./9.])
+        b = np.array([1./3., 1./3., 1./9.])
+        self.assertTrue(np.all(np.isclose(crystal.incell(a), b)))
+
+    def testhalfcell(self):
+        """Half cell testing"""
+        a = np.array([4./3., -2./3.,17./9.])
+        b = np.array([1./3., 1./3., -1./9.])
+        self.assertTrue(np.all(np.isclose(crystal.inhalf(a), b)))
+
+
 class CrystalClassTests(unittest.TestCase):
     """Tests for the crystal class and symmetry analysis."""
 
@@ -18,9 +33,7 @@ class CrystalClassTests(unittest.TestCase):
         self.bcclatt = self.a0*np.array([[-0.5, 0.5, 0.5], [0.5, -0.5, 0.5], [0.5, 0.5, -0.5]])
         self.basis = [np.array([0.,0.,0.])]
 
-    def testscMetric(self):
-        """Does the simple cubic lattice have the right volume and metric?"""
-        crys = crystal.Crystal(self.sclatt, self.basis)
+    def isscMetric(self, crys):
         self.assertAlmostEqual(crys.volume, self.a0**3)
         for i, a2 in enumerate(crys.metric.flatten()):
             if i%4 == 0:
@@ -30,9 +43,7 @@ class CrystalClassTests(unittest.TestCase):
                 # off-diagonal element
                 self.assertAlmostEqual(a2, 0)
 
-    def testfccMetric(self):
-        """Does the face-centered cubic lattice have the right volume and metric?"""
-        crys = crystal.Crystal(self.fcclatt, self.basis)
+    def isfccMetric(self, crys):
         self.assertAlmostEqual(crys.volume, 0.25*self.a0**3)
         for i, a2 in enumerate(crys.metric.flatten()):
             if i%4 == 0:
@@ -42,9 +53,7 @@ class CrystalClassTests(unittest.TestCase):
                 # off-diagonal element
                 self.assertAlmostEqual(a2, 0.25*self.a0**2)
 
-    def testbccMetric(self):
-        """Does the body-centered cubic lattice have the right volume and metric?"""
-        crys = crystal.Crystal(self.bcclatt, self.basis)
+    def isbccMetric(self, crys):
         self.assertAlmostEqual(crys.volume, 0.5*self.a0**3)
         for i, a2 in enumerate(crys.metric.flatten()):
             if i%4 == 0:
@@ -53,3 +62,34 @@ class CrystalClassTests(unittest.TestCase):
             else:
                 # off-diagonal element
                 self.assertAlmostEqual(a2, -0.25*self.a0**2)
+
+    def testscMetric(self):
+        """Does the simple cubic lattice have the right volume and metric?"""
+        crys = crystal.Crystal(self.sclatt, self.basis)
+        self.isscMetric(crys)
+        self.assertEqual(len(crys.basis), 1)    # one chemistry
+        self.assertEqual(len(crys.basis[0]), 1) # one atom in the unit cell
+
+    def testfccMetric(self):
+        """Does the face-centered cubic lattice have the right volume and metric?"""
+        crys = crystal.Crystal(self.fcclatt, self.basis)
+        self.isfccMetric(crys)
+        self.assertEqual(len(crys.basis), 1)    # one chemistry
+        self.assertEqual(len(crys.basis[0]), 1) # one atom in the unit cell
+
+    def testbccMetric(self):
+        """Does the body-centered cubic lattice have the right volume and metric?"""
+        crys = crystal.Crystal(self.bcclatt, self.basis)
+        self.isbccMetric(crys)
+        self.assertEqual(len(crys.basis), 1)    # one chemistry
+        self.assertEqual(len(crys.basis[0]), 1) # one atom in the unit cell
+
+    def testscReduce(self):
+        """If we start with a supercell, does it get reduced back to our start?"""
+        nsuper = np.array([[2,0,0],[0,1,0],[0,0,1]], dtype=int)
+        doublebasis = [self.basis[0], np.array([0.5, 0, 0]) + self.basis[0]]
+        crys = crystal.Crystal(np.dot(self.sclatt, nsuper), doublebasis)
+        crys.lattice = 0
+        self.isscMetric(crys)
+        self.assertEqual(len(crys.basis), 1)    # one chemistry
+        self.assertEqual(len(crys.basis[0]), 1) # one atom in the unit cell
