762138c052508e51ecaf7389404fa6e763f9ad32
DallasTrinkle <dtrinkle@illinois.edu>
2015-03-14 17:01:28 -0500

    Quick refactor on YAMLsimplified test to explicitly call back to the crystal.NDARRAY_YAMLTAG rather than hard-coding it.

--- test/test_crystal.py
+++ test/test_crystal.py
@@ -492,16 +492,16 @@ class YAMLTests(unittest.TestCase):
                   np.array([2./3.,1./3.,0.5])]]
         crys = crystal.Crystal(hexlatt, basis)
         yamlstr = """lattice_constant: 2.5
-lattice: !numpy.ndarray
+lattice: {YAMLtag}
 - [0.5, -0.8660254037844386, 0]
 - [0.5,  0.8660254037844386, 0]
 - [0.0, 0.0, 1.6329931618554521]
 basis:
-- - !numpy.ndarray [0.0, 0.0, 0.0]
-  - !numpy.ndarray [0.3333333333333333, 0.6666666666666666, 0.5]
-  - !numpy.ndarray [0.6666666666666666, 0.3333333333333333, 0.5]
+- - {YAMLtag} [0.0, 0.0, 0.0]
+  - {YAMLtag} [0.3333333333333333, 0.6666666666666666, 0.5]
+  - {YAMLtag} [0.6666666666666666, 0.3333333333333333, 0.5]
 chemistry:
-- Ti"""
+- Ti""".format(YAMLtag = crystal.NDARRAY_YAMLTAG) # rather than hard-coding the tag...
         crysread = crystal.Crystal.fromdict(crystal.yaml.load(yamlstr))
         self.assertIsInstance(crysread, crystal.Crystal)
         self.assertTrue(np.all(np.isclose(crys.lattice, crysread.lattice)))
