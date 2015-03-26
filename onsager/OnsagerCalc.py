Bias corrected--not an issue with elastodiffusion tensor, but computation of VV was *not correct*--needed to refactor as a 3x3xNVxNV matrix to catch off-diagonal terms (when a site has more than one vector basis), also allows us to use np.dot.

onsager/OnsagerCalc.py
@@ -121,7 +121,6 @@ class Interstitial(object):
                                    np.array([0.,0.,1.])]
 
         lis = []
-        lisVV = []
         for s in self.sitelist:
             for v in vectlist(self.crys.VectorBasis((self.chem, s[0]))):
                 v /= np.sqrt(len(s)) # additional normalization
@@ -131,8 +130,12 @@ class Interstitial(object):
                     # what site do we land on, and what's the vector? (this is slight overkill)
                     vb[g.indexmap[self.chem][s[0]]] = self.crys.g_direc(g, v)
                 lis.append(vb)
-                lisVV.append(np.dot(vb.T, vb))
-        return lis, lisVV
+        # need the *full matrix of this tensor*
+        VV = np.zeros((3,3,len(lis),len(lis)))
+        for i,vb_i in enumerate(lis):
+            for j,vb_j in enumerate(lis):
+                VV[:,:,i,j] = np.dot(vb_i.T, vb_j)
+        return lis, VV
 
     def generateSiteGroupOps(self):
         """
@@ -297,8 +300,7 @@ class Interstitial(object):
             else:
                 # pseudoinverse required:
                 gamma_v = np.dot(pinv2(omega_v), bias_v)
-            for b, g, VV in zip(bias_v, gamma_v, self.VV):
-                D0 += b*g*VV
+            D0 += np.dot(np.dot(self.VV, bias_v), gamma_v)
         return D0
 
     def elastodiffusion(self, pre, betaene, dipole, preT, betaeneT, dipoleT):
@@ -367,8 +369,7 @@ class Interstitial(object):
             else:
                 # pseudoinverse required:
                 gamma_v = np.dot(pinv2(omega_v), bias_v)
-            for b, g, VV in zip(bias_v, gamma_v, self.VV):
-                D0 += b*g*VV
+            D0 += np.dot(np.dot(self.VV, bias_v), gamma_v)
 
         for a,b,c,d in ((a,b,c,d) for a in xrange(3) for b in xrange(3) for c in xrange(3) for d in xrange(3)):
             if a==d:

test/test_OnsagerCalc.py
@@ -311,7 +311,7 @@ class InterstitialTests(unittest.TestCase):
         """Do we correctly analyze our crystals regarding their symmetry?"""
         self.assertEqual(self.Dhcp.NV, 1)
         self.assertTrue(self.Dhcp.omega_invertible)
-        self.assertTrue(np.allclose(self.Dhcp.VV[0], np.array([[0,0,0],[0,0,0],[0,0,1]])))
+        self.assertTrue(np.allclose(self.Dhcp.VV[:,:,0,0], np.array([[0,0,0],[0,0,0],[0,0,1]])))
         self.assertEqual(self.Dfcc.NV, 0)
         self.assertTrue(self.Dfcc.omega_invertible)
 
@@ -574,14 +574,13 @@ class InterstitialTests(unittest.TestCase):
 
     def testHCPElastodiffusion(self):
         """Elastodiffusion tensor with correlation; compare with finite difference"""
-        # HCP first:
-        # HCP
+        # HCP; note: *uncorrelated* requires lambda(t->t) = 1.5*lambda(t->o)
         preoct = 1.
         pretet = 0.5
         BEoct = 0.
         BEtet = np.log(2) # so exp(-beta*E) = 1/2
         preTransOT = 10.
-        preTransTT = 15
+        preTransTT = 100.
         BETransOT = np.log(10.)
         BETransTT = np.log(10.)
 
@@ -653,11 +652,11 @@ class InterstitialTests(unittest.TestCase):
                 else:
                     strainedpreT[ind] = preTransOT
                     strainedBET[ind] = BETransOT + np.sum(dip*strainmat)
-            print strainedHCP_sitelist
-            print strainedBE
-            print strainedpre
-            print strainedBET
-            print strainedpreT
+            # print strainedHCP_sitelist
+            # print strainedBE
+            # print strainedpre
+            # print strainedBET
+            # print strainedpreT
             Deps = strainedDhcp.diffusivity(strainedpre, strainedBE, strainedpreT, strainedBET)
             Deps0 = np.tensordot(Dp, strainmat, axes=((2,3), (0,1)))/eps
             failmsg = """