Fixed rotation issue, so that it now works with reduce, too. Whew!

onsager/PowerExpansion.py
@@ -308,9 +308,15 @@ class Taylor3D(object):
                                         powtrans[powijk,cls.directmult[pij,pk]] += powtrans[powij,pij]*powtrans[powk,pk]
         npowtrans = np.zeros((cls.Lmax+1, cls.Npower, cls.Npower))
         for n in range(cls.Lmax+1):
-            p0 = cls.powlrange[n-1]
-            p1 = cls.powlrange[n]
-            npowtrans[n,p0:p1,p0:p1] = powtrans[p0:p1,p0:p1]
+            prange = slice(cls.powlrange[n-1], cls.powlrange[n])
+            npowtrans[n,prange,prange] = powtrans[prange,prange]
+            # now, work on lower values (n-2, n-4, ...)
+            for m in range(n-2,-1,-2):
+                # powers that sum up to m:
+                for tup in [(n0,n1,m-n0-n1) for n0 in range(m+1) for n1 in range(m-n0+1)]:
+                    npowtrans[n,cls.pow2ind[tup],:] = npowtrans[n,cls.pow2ind[tup[0]+2,tup[1],tup[2]],:] + \
+                                                      npowtrans[n,cls.pow2ind[tup[0],tup[1]+2,tup[2]],:] + \
+                                                      npowtrans[n,cls.pow2ind[tup[0],tup[1],tup[2]+2],:]
         return npowtrans
 
     # for sorting our coefficient lists:
@@ -523,20 +529,20 @@ class Taylor3D(object):
         return ca
 
     @classmethod
-    def rotatecoeff(cls, a, powtrans, inplace=False):
+    def rotatecoeff(cls, a, npowtrans, inplace=False):
         """
         Return a rotated version of the expansion.
         :param a: coefficiant list
-        :param powtrans: Npow x Npow matrix, of [oldpow,newpow] corresponding to the rotation
+        :param npowtrans: Lmax+1 x Npow x Npow matrix, of [n,oldpow,newpow] corresponding to the rotation
         :return: coefficient list, rotated
         """
         acoeff = getattr(a, 'coefflist', a)
         if not inplace:
-            return [(n,l,np.tensordot(powtrans[n,:cls.powlrange[l],:cls.powlrange[l]], c, axes=(0,0)))
+            return [(n,l,np.tensordot(npowtrans[n,:cls.powlrange[l],:cls.powlrange[l]], c, axes=(0,0)))
                     for n,l,c in acoeff]
         else:
             for i,(n,l,c) in enumerate(acoeff):
-                acoeff[i] = (n,l,np.tensordot(powtrans[n,:cls.powlrange[l],:cls.powlrange[l]], c,
+                acoeff[i] = (n,l,np.tensordot(npowtrans[n,:cls.powlrange[l],:cls.powlrange[l]], c,
                                               axes=(0,0)))
             return acoeff
 

test/test_PowerExpansion.py
@@ -372,6 +372,7 @@ class PowerExpansionTests(unittest.TestCase):
                 self.assertAlmostEqual(crot(u, fnu)[0,0], crotdirect(u, fnu)[0,0],
                                        msg="Failed before reduce()")
         # now, a more detailed test: do a reduce.
+        c2 = c.copy()
         c.reduce()
         for rot in [np.eye(3), 2.*np.eye(3), 0.5*np.eye(3),
                     np.array([[1.25,0.5,0.25],[-0.25,0.9,0.5],[-0.75,-0.4,0.6]]),
@@ -384,8 +385,10 @@ class PowerExpansionTests(unittest.TestCase):
                        np.array([0., 0., 1.2]),
                        np.array([0.234, -0.5, 0.5]),
                        np.array([-0.24, 0.41, -1.3])]:
+                self.assertAlmostEqual(c(u, fnu)[0,0], c2(u, fnu)[0,0],
+                                       msg="Failure in reduce() to produce equal function values?")
                 self.assertAlmostEqual(crot(u, fnu)[0,0], crotdirect(u, fnu)[0,0],
-                                       msg="Failed after reduce()")
+                                       msg="Failed after reduce() for\n{}".format(rot))
 
 
 