da8636f259cfb7c69c8d7d07d6103db5f1c24197
DallasTrinkle <dtrinkle@illinois.edu>
2015-12-21 02:12:22 -0600

    Typo fix in change-over to np.allclose in testing.

--- test/test_OnsagerCalc.py
+++ test/test_OnsagerCalc.py
@@ -110,8 +110,7 @@ class BaseTests(unittest.TestCase):
                         vsum = gv + v2
                         match = False
                         for vGF in GFlist:
-                            if np.dot(vGF, vGF) != np.dot(vsum, vsum):
-                                continue
+                            if not np.isclose(np.dot(vGF, vGF), np.dot(vsum, vsum)): continue
                             if any([np.allclose(vsum,np.dot(g, vGF), atol=1e-8) for g in self.groupops]):
                                 match = True
                                 break
