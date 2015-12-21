d9608f3f6077fc45f25abf18d29ab7e3400a72d5
DallasTrinkle <dtrinkle@illinois.edu>
2015-12-21 11:24:39 -0600

    Edit to setup.

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 setup(
     name = 'onsager',
-    packages = ['onsager', 'test'], # including the test?
-    version = '0.2.1',
+    packages = ['onsager'], 
+    version = '0.3',
     description = 'A package to compute Onsager coefficients for vacancy-mediated diffusion and interstitial elastodiffusion tensors',
     author = 'Dallas R. Trinkle',
     author_email = 'dtrinkle@illinois.edu',
     license='LICENSE.txt',
     url = 'https://github.com/DallasTrinkle/onsager', # use the URL to the github repo
-    download_url = 'https://github.com/DallasTrinkle/onsager/tarball/0.2', # for when we upload
+    download_url = 'https://github.com/DallasTrinkle/onsager/tarball/0.3', # for when we upload
     keywords = ['diffusion', 'elastodiffusion', 'Onsager', 'mass transport'],
-    classifiers = [], requires=['numpy']
+    classifiers = [], requires=['numpy', 'scipy']
 )
