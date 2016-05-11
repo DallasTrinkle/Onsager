27102fb4eccdae4e820ed79630a2bf7a2bab656a
DallasTrinkle <dtrinkle@illinois.edu>
2016-05-11 13:46:30 -0500

    Starting to work out the supercell objects.

--- onsager/supercell.py
+++ onsager/supercell.py
@@ -20,3 +20,19 @@ from functools import reduce
 # interfaces are either at the bottom, or staticmethods in the corresponding object
 # NDARRAY_YAMLTAG = '!numpy.ndarray'
 # GROUPOP_YAMLTAG = '!GroupOp'
+
+class Supercell(object):
+    """
+    A class that defines a Supercell of a crystal
+    """
+    def __init__(self, crys, super, empty=False):
+        """
+        Initialize our supercell
+
+        :param crys: crystal object
+        :param super: 3x3 integer matrix
+        :param empty: optional; designed to allow "copy" to work
+        """
+        if empty: return
+        self.crys = crys
+        self.super = super.copy()
--- test/test_supercell.py
+++ test/test_supercell.py
@@ -7,7 +7,14 @@ __author__ = 'Dallas R. Trinkle'
 import unittest
 import numpy as np
 import onsager.crystal as crystal
-import onsager.supercell as super
+import onsager.supercell as supercell
 
 class TypeTests(unittest.TestCase):
     """Tests to make sure we can make a supercell object."""
+    def setUp(self):
+        self.crys = crystal.Crystal.FCC(1.,'Al')
+
+    def testSuper(self):
+        """Can we make a supercell object?"""
+        super = supercell.Supercell(self.crys, np.eye(3, dtype=int))
+        self.assertNotEqual(super, None)
