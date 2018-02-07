c7663d4eaf806494fca38c5d53913d5ab3230344
DallasTrinkle <dtrinkle@illinois.edu>
2018-02-07 16:54:07 -0500

    Cleanup to avoid deprecation warnings in test_PowerExpansion

--- test/test_PowerExpansion.py
+++ test/test_PowerExpansion.py
@@ -38,33 +38,29 @@ class PowerExpansionTests(unittest.TestCase):
             utest, umagn = T3D.powexp(np.array([np.sin(phi) * np.cos(theta),
                                                 np.sin(phi) * np.sin(theta),
                                                 np.cos(phi)]))
-            self.assertAlmostEquals(umagn, 1)
+            self.assertAlmostEqual(umagn, 1)
             Ylm0 = np.zeros(T3D.NYlm, dtype=complex)
             # Ylm as power expansions
             for lm in range(T3D.NYlm):
                 l, m = T3D.ind2Ylm[lm, 0], T3D.ind2Ylm[lm, 1]
                 Ylm0[lm] = sph_harm(m, l, theta, phi)
                 Ylmexp = np.dot(T3D.Ylmpow[lm], utest)
-                self.assertAlmostEquals(Ylm0[lm], Ylmexp,
-                                        msg="Failure for Ylmpow l={} m={}; theta={}, phi={}\n{} != {}".format(l, m,
-                                                                                                              theta,
-                                                                                                              phi,
-                                                                                                              Ylm0[lm],
-                                                                                                              Ylmexp))
+                self.assertAlmostEqual(Ylm0[lm], Ylmexp,
+                                       msg="Failure for Ylmpow " 
+                    "l={} m={}; theta={}, phi={}\n{} != {}".format(l, m, theta, phi, Ylm0[lm], Ylmexp))
             # power expansions in Ylm's
             for p in range(T3D.NYlm):
                 pYlm = np.dot(T3D.powYlm[p], Ylm0)
-                self.assertAlmostEquals(utest[p], pYlm,
-                                        msg="Failure for powYlm {}; theta={}, phi={}\n{} != {}".format(T3D.ind2pow[p],
-                                                                                                       theta, phi,
-                                                                                                       utest[p], pYlm))
+                self.assertAlmostEqual(utest[p], pYlm,
+                                       msg="Failure for powYlm " 
+                    "{}; theta={}, phi={}\n{} != {}".format(T3D.ind2pow[p], theta, phi, utest[p], pYlm))
             # projection (note that Lproj is not symmetric): so this test ensures that v.u and (proj.v).u
             # give the same value
             uproj = np.tensordot(T3D.Lproj[-1], utest, axes=(0, 0))
             for p in range(T3D.NYlm):
-                self.assertAlmostEquals(utest[p], uproj[p],
-                                        msg="Projection failure for {}\n{} != {}".format(T3D.ind2pow[p], uproj[p],
-                                                                                         utest[p]))
+                self.assertAlmostEqual(utest[p], uproj[p],
+                                       msg="Projection failure for " 
+                    "{}\n{} != {}".format(T3D.ind2pow[p], uproj[p], utest[p]))
 
     def testProjection(self):
         """Test that the L-projections are correct"""
