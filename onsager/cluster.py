Quick fix in nn calculation.

onsager/cluster.py
@@ -315,7 +315,7 @@ def makeclusters(crys, cutoff, maxorder, exclude=()):
     # first, make lists of all our pairs within a given nn distance
     # we could modify this to use different cutoff between different chemistries...
     r2 = cutoff * cutoff
-    nmax = [int(np.round(np.sqrt(crys.metric[i, i]))) + 1
+    nmax = [int(np.round(np.sqrt(r2/crys.metric[i, i]))) + 1
             for i in range(crys.dim)]
     nranges = [range(-n, n+1) for n in nmax]
     supervect = [np.array(ntup) for ntup in itertools.product(*nranges)]

onsager/crystal.py
@@ -1394,7 +1394,7 @@ class Crystal(object):
         :return nnlist: list of nearest neighbor vectors
         """
         r2 = cutoff * cutoff
-        nmax = [int(np.round(np.sqrt(self.metric[i, i]))) + 1
+        nmax = [int(np.round(np.sqrt(r2/self.metric[i, i]))) + 1
                 for i in range(self.dim)]
         nranges = [range(-n, n+1) for n in nmax]
         supervect = [np.array(ntup) for ntup in itertools.product(*nranges)]